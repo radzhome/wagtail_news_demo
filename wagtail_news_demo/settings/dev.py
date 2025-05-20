c7392958fa1fa708cce6d058eda7d7a3fd0c6c05
rad <radzhome@gmail.com>
2025-05-20 10:40:27 -0400

    init commi t

---
+++ wagtail_news_demo/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-p1pv1p68q6^q_bstu4d01l*l$8i)ra%7zf4a_u6stk2adz-p4%"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ wagtail_news_demo/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for wagtail_news_demo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagtail_news_demo.settings.production")
+
+application = get_wsgi_application()
